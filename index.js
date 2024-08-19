2f84a0ec1b0c1cae5d25136185105490e6ddc3ec
Nadine Grantz <nadine.grantz@googlemail.com>
2024-08-19 13:39:42 +0200

    change rainforest structure

--- index.js
+++ index.js
@@ -12,35 +12,35 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelector(".dynamic-nav").innerHTML = navHTML;
 
    
-   const adviceList = [
-    {
-      title: "Buy certified coffee",
-      advice: "Look out for certificates such as Fair Trade, Rainforest Alliance or organic to ensure that the coffee has been grown in a resource-friendly way."
-    },
-    {
-      title: "Avoid disposable cups",
-      advice: "Use reusable coffee cups to reduce waste and protect the environment"
-    },
-    {
-      title: "Choose local roasters",
-      advice: "Support local coffee roasters who are committed to sustainable cultivation and fair working conditions"
-    }
-  ];
+//    const adviceList = [
+//     {
+//       title: "Buy certified coffee",
+//       advice: "Look out for certificates such as Fair Trade, Rainforest Alliance or organic to ensure that the coffee has been grown in a resource-friendly way."
+//     },
+//     {
+//       title: "Avoid disposable cups",
+//       advice: "Use reusable coffee cups to reduce waste and protect the environment"
+//     },
+//     {
+//       title: "Choose local roasters",
+//       advice: "Support local coffee roasters who are committed to sustainable cultivation and fair working conditions"
+//     }
+//   ];
 
-  const container = document.getElementById('advice-container');
+//   const container = document.getElementById('advice-container');
 
-  adviceList.forEach(item => {
-    const card = document.createElement('div');
-    card.className = 'card';
+//   adviceList.forEach(item => {
+//     const card = document.createElement('div');
+//     card.className = 'card';
 
-    const cardTitle = document.createElement('h2');
-    cardTitle.textContent = item.title;
-    card.appendChild(cardTitle);
+//     const cardTitle = document.createElement('h2');
+//     cardTitle.textContent = item.title;
+//     card.appendChild(cardTitle);
 
-    const cardAdvice = document.createElement('p');
-    cardAdvice.textContent = item.advice;
-    card.appendChild(cardAdvice);
+//     const cardAdvice = document.createElement('p');
+//     cardAdvice.textContent = item.advice;
+//     card.appendChild(cardAdvice);
 
-    container.appendChild(card); 
-  });
-});
+//     container.appendChild(card); 
+//   });
+// });
