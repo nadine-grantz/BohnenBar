66d3702b4f012fb77d6d7e78b3b13c145fd35d90
Nadine Grantz <nadine.grantz@googlemail.com>
2024-08-25 00:10:55 +0200

    git add .

--- pages/workshop/workshops.js
+++ pages/workshop/workshops.js
@@ -28,7 +28,7 @@ const cafeWorkshops = [
     name: "Latte Art Workshop",
     image: "https://images.unsplash.com/photo-1551266681-ba5f0b95e2e5?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 25,
-    category: "Craft"
+    category: "Bean Special"
   },
   {
     name: "Board Game Tournament",
@@ -41,12 +41,6 @@ const cafeWorkshops = [
     image: "https://images.unsplash.com/photo-1539021897569-06e9fa3c6bb9?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     price: 30,
     category: "Bean Special"
-  },
-  {
-    name: "Karaoke Night",
-    image: "https://images.unsplash.com/photo-1693835843843-82f691094f59?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    price: 12,
-    category: "Art"
   },
   {
     name: "Handmade Bracelet Workshop",
@@ -85,12 +79,6 @@ const cafeWorkshops = [
     category: "Bean Special"
   },
   {
-    name: "Open Mic Night",
-    image: "https://images.unsplash.com/photo-1648808678096-18f488fd6858?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8b3BlbiUyMG1pY3xlbnwwfHwwfHx8Mg%3D%3D",
-    price: 10,
-    category: "Art"
-  },
-  {
     name: "Macrame Workshop",
     image: "https://images.unsplash.com/photo-1590494075205-9d76c8784f11?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fE1hY3JhbWV8ZW58MHx8MHx8fDI%3D",
     price: 20,
@@ -105,7 +93,7 @@ const cafeWorkshops = [
   {
     name: "Sustainable Fashion Talk",
     image: "https://images.unsplash.com/photo-1596097198305-e4844a56ddb8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3VzdGFpbmFibGUlMjBmYXNoaW9ufGVufDB8fDB8fHwy",
-    price: 10,
+    price: 0,
     category: "Textile"
   }
 ];
